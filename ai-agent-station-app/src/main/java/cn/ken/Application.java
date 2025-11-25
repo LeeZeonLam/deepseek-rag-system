6c04d6a268c0857728a5ef4c01c634eb1864ccf8
leezeonlam <493282261@qq.com>
2025-11-25 15:00:17 +0800

    feat: init project success

--- ai-agent-station-app/src/main/java/cn/ken/Application.java
+++ ai-agent-station-app/src/main/java/cn/ken/Application.java
@@ -1,4 +1,16 @@
 package cn.ken;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configurable
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
 }
